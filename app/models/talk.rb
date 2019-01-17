e6de0e7ddf9920520cd78c8b970b77b8e13e7e43
Christoffer Artmann <artgaard@gmail.com>
2019-01-17 19:53:24 +0100

    Parse poster URL from talk url

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -10,6 +10,9 @@ class Talk
   end
 
   def poster_url
-    "https://carepharmaceuticals.com.au/wp-content/uploads/sites/19/2018/02/placeholder-600x400.png"
+    matches = /youtube.com\/embed\/([A-Za-z0-9]+)/.match(self.embed)
+    return 'https://carepharmaceuticals.com.au/wp-content/uploads/sites/19/2018/02/placeholder-600x400.png' if matches[1].nil?
+
+    return "https://img.youtube.com/vi/#{matches[1]}/0.jpg"
   end
 end
