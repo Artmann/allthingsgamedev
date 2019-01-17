e178e49f90ca65c3284cc539c3854cfc0c9f1f13
Christoffer Artmann <artgaard@gmail.com>
2019-01-17 22:15:32 +0100

    Lazy load article images

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,2 +1,60 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+const lazyLoadOnIntersection = function (lazyImages) {
+  const lazyImageObserver = new IntersectionObserver(function (entries, observer) {
+    entries.forEach(function (entry) {
+      if (entry.isIntersecting) {
+        const lazyImage = entry.target;
+        lazyImage.src = lazyImage.dataset.src;
+        lazyImage.classList.remove('lazy');
+        lazyImageObserver.unobserve(lazyImage);
+      }
+    });
+  });
+
+  lazyImages.forEach(function (lazyImage) {
+    lazyImageObserver.observe(lazyImage);
+  });
+};
+
+let active = false;
+const legacyLazyLoad = function (lazyImages) {
+  if (active === false) {
+    active = true;
+
+    setTimeout(function () {
+      lazyImages.forEach(function (lazyImage) {
+        if ((lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) && getComputedStyle(lazyImage).display !== 'none') {
+          lazyImage.src = lazyImage.dataset.src;
+          lazyImage.classList.remove("lazy");
+
+          lazyImages = lazyImages.filter(function (image) {
+            return image !== lazyImage;
+          });
+
+          if (lazyImages.length === 0) {
+            document.removeEventListener("scroll", lazyLoad);
+            window.removeEventListener("resize", lazyLoad);
+            window.removeEventListener("orientationchange", lazyLoad);
+          }
+        }
+      });
+
+      active = false;
+    }, 200);
+  }
+};
+
+document.addEventListener('turbolinks:load', function () {
+  let lazyImages = [].slice.call(document.querySelectorAll('img.lazy'));
+
+  if ('IntersectionObserver' in window) {
+    lazyLoadOnIntersection(lazyImages);
+  } else {
+    const lazyLoad = function () {
+      legacyLazyLoad(lazyImages);
+    };
+
+    document.addEventListener('scroll', lazyLoad);
+    window.addEventListener('resize', lazyLoad);
+    window.addEventListener('orientationchange', lazyLoad);
+  }
+});
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,14 @@
+class CustomRender < Redcarpet::Render::HTML
+    def image(link, title, alt_text)
+        %(<img alt="#{alt_text}" class="lazy" data-src="#{link}" title="#{title}" src="iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M9QDwADhgGAWjR9awAAAABJRU5ErkJggg==" />)
+    end
+end
+
 module ApplicationHelper
     def markdown(text)
         return '' if text.nil?
 
-        renderer = Redcarpet::Markdown.new(Redcarpet::Render::HTML, fenced_code_blocks: true)
+        renderer = Redcarpet::Markdown.new(CustomRender, fenced_code_blocks: true)
         doc = Nokogiri::HTML::DocumentFragment.parse(renderer.render(text))
         doc.css('code[@class]').each do |code|
             div = CodeRay.scan(code.text.rstrip, code[:class].to_sym).div
