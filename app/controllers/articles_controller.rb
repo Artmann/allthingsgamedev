2ce760c5d0e77b6ec4f20498fedc695a03564a17
Christoffer Artmann <Artgaard@gmail.com>
2020-09-20 14:57:29 +0200

    feat: Use auto ads.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,27 +15,6 @@ class ArticlesController < ApplicationController
 
         doc = Nokogiri::HTML::DocumentFragment.parse(renderer.render(@article.content))
 
-        ad_ids = [
-            'div-gpt-ad-1547765304248-0',
-            'div-gpt-ad-1547765393475-0',
-            'div-gpt-ad-1547765417792-0'
-        ]
-        index = 0
-        word_count = 0
-
-        doc.css('p').each do |block|
-            word_count += (block.text || "").split(" ").size
-
-            if word_count >= 150
-                id = ad_ids[index % ad_ids.size]
-                index += 1
-
-                block.add_next_sibling "<div id='#{id}'><script>googletag.cmd.push(function() { googletag.display('#{id}'); });</script></div>"
-
-                word_count = 0
-            end
-        end
-
         @articleContent = doc.to_s
     end
 end
