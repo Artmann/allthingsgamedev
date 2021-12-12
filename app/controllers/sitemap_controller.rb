b4a30a553d06a8806c6f3774d4bfe1b1bc190988
Christoffer Artmann <Artgaard@gmail.com>
2021-12-12 11:08:44 +0100

    feat: Add talks to sitemap.

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -1,5 +1,6 @@
 class SitemapController < ApplicationController
   def index
     @articles = ContentService.instance.list_articles.sort { |a, b| b.created_at <=> a.created_at }
+    @talks = ContentService.instance.list_talks.sort { |a, b| b.created_at <=> a.created_at }
   end
 end
