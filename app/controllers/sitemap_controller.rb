e594a3891166aed5dc8bbda9cb66f014ce4e55ab
Christoffer Artmann <Artgaard@gmail.com>
2021-12-12 10:58:31 +0100

    fix: Use the signleton contenful service insteado f instansiating a new one.

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -1,5 +1,5 @@
 class SitemapController < ApplicationController
   def index
-    @articles = ContentService.new.list_articles.sort { |a, b| b.created_at <=> a.created_at }
+    @articles = ContentService.instance.list_articles.sort { |a, b| b.created_at <=> a.created_at }
   end
 end
